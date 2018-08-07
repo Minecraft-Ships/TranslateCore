9015e354bb9afc89f33bfe2fc9b193648470749f
Mose <Nekronmoseley@gmail.com>
2018-08-07 13:56:51 +0100

    Added BlockType

--- src/org/core/world/position/block/BlockType.java
+++ src/org/core/world/position/block/BlockType.java
@@ -1,8 +1,18 @@
 package org.core.world.position.block;
 
 import org.core.utils.Identifable;
+import org.core.world.position.block.details.BlockDetails;
+
+import java.util.Set;
 
 public interface BlockType extends Identifable {
 
+    BlockDetails getDefaultBlockDetails();
+
+    Set<BlockType> getLike();
+
+    default boolean isLike(BlockType type) {
+        return getLike().stream().anyMatch(t -> t.equals(type));
+    }
 
 }
