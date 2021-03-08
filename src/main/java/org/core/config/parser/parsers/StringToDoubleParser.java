6c364d65dfe099403b7834bc6dcff541b5a70c3f
mosemister <nekronmoseley@gmail.com>
2021-03-08 22:32:41 +0000

    Fixed git issue

--- src/main/java/org/core/config/parser/parsers/StringToDoubleParser.java
+++ src/main/java/org/core/config/parser/parsers/StringToDoubleParser.java
@@ -4,26 +4,25 @@ import org.core.config.parser.StringParser;
 
 import java.util.Optional;
 
-public class StringToDoubleParser implements StringParser<Double>{
+public class StringToDoubleParser implements StringParser<Double> {
     @Override
     public Optional<Double> parse(String original) {
         try {
             double parsedDouble = Double.parseDouble(original);
             return Optional.of(parsedDouble);
         } catch (NumberFormatException e) {
-            int parsedInt = Integer.parseInt(original);
-            return Optional.of((double) parsedInt);
+            return Optional.empty();
         }
     }
 
     @Override
     public String unparse(Double value) {
-        if(value == null){
+        if (value == null) {
             return "" + 0;
         }
         double value2 = value;
-        if((int)value2 == value2){
-           return "" + ((int)value2);
+        if ((int) value2 == value2) {
+            return "" + ((int) value2);
         }
         return "" + value;
     }
