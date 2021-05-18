7858c3de40ecf07c07cee302d1aa2a5dbc1b5a66
mosemister <nekronmoseley@gmail.com>
2021-05-18 22:38:22 +0100

    More messages converted

--- src/main/java/org/core/utils/Else.java
+++ src/main/java/org/core/utils/Else.java
@@ -1,11 +1,20 @@
 package org.core.utils;
 
+import java.util.function.Function;
+
 public interface Else {
 
     interface ThrowableConsumer<F, E extends Throwable> {
 
         F run() throws E;
 
+    }
+
+    static <O, E extends O, F> F canCast(O obj, Class<E> castTo, Function<E, F> casted, Function<O, F> fail) {
+        if (castTo.isInstance(obj)) {
+            return casted.apply((E) obj);
+        }
+        return fail.apply(obj);
     }
 
     static <F, E extends Throwable> F throwOr(Class<E> clazz, ThrowableConsumer<F, E> consumer, F fail) {
