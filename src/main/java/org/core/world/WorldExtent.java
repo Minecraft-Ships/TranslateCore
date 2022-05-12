7e59625cfb5444dbf405da1c838a9cdd7d19cbb5
mosemister <nekronmoseley@gmail.com>
2022-05-12 21:11:07 +0100

    Testing some water fixes

--- src/main/java/org/core/world/WorldExtent.java
+++ src/main/java/org/core/world/WorldExtent.java
@@ -9,9 +9,16 @@ import java.util.UUID;
 public interface WorldExtent extends Extent {
 
     String getName();
+
     UUID getUniqueId();
+
     String getPlatformUniqueId();
+
     Set<ChunkExtent> getChunks();
+
     Optional<ChunkExtent> getChunk(Vector3<Integer> vector);
+
     ChunkExtent loadChunk(Vector3<Integer> vector);
+
+    int getMinimumBlockHeight();
 }
