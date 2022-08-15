2780fbcc736e27390bb2424274b82114a2770993
mosemister <nekronmoseley@gmail.com>
2022-08-15 19:22:42 +0100

    Fixed Fixed airship

--- src/main/java/org/core/world/position/block/entity/container/furnace/LiveFurnaceTileEntity.java
+++ src/main/java/org/core/world/position/block/entity/container/furnace/LiveFurnaceTileEntity.java
@@ -1,7 +1,10 @@
 package org.core.world.position.block.entity.container.furnace;
 
+import org.core.inventory.inventories.live.block.LiveFurnaceInventory;
 import org.core.world.position.block.entity.LiveTileEntity;
 
 public interface LiveFurnaceTileEntity extends FurnaceTileEntity, LiveTileEntity {
 
+    @Override
+    LiveFurnaceInventory getInventory();
 }
