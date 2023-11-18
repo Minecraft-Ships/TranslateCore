35362d1e125b362d6dc930f4d968015ebbb81549
mose <nekronmoseley@gmail.com>
2023-11-18 10:09:25 +0000

    boolean on teleport now

--- src/main/java/org/core/command/argument/arguments/operation/StringParserArgument.java
+++ src/main/java/org/core/command/argument/arguments/operation/StringParserArgument.java
@@ -2,6 +2,7 @@ package org.core.command.argument.arguments.operation;
 
 import org.core.command.argument.CommandArgument;
 import org.core.command.argument.CommandArgumentResult;
+import org.core.command.argument.ParseCommandArgument;
 import org.core.command.argument.context.CommandArgumentContext;
 import org.core.command.argument.context.CommandContext;
 import org.core.config.parser.StringParser;
@@ -15,12 +16,12 @@ import java.util.function.BiFunction;
 public class StringParserArgument<P> implements CommandArgument<P> {
 
     private final String id;
-    private final BiFunction<? super CommandContext, ? super CommandArgumentContext<P>, ? extends StringParser<P>> function;
+    private final ParseCommandArgument<StringParser<P>> function;
     private final BiFunction<? super CommandArgumentContext<P>, ? super StringParser<P>, String> failMessage;
 
     public StringParserArgument(String id,
-            BiFunction<? super CommandContext, ? super CommandArgumentContext<P>, ? extends StringParser<P>> function,
-            BiFunction<? super CommandArgumentContext<P>, ? super StringParser<P>, String> failMessage) {
+                                ParseCommandArgument<StringParser<P>> function,
+                                BiFunction<? super CommandArgumentContext<P>, ? super StringParser<P>, String> failMessage) {
         this.id = id;
         this.function = function;
         this.failMessage = failMessage;
@@ -32,9 +33,11 @@ public class StringParserArgument<P> implements CommandArgument<P> {
     }
 
     @Override
-    public CommandArgumentResult<P> parse(CommandContext context, CommandArgumentContext<P> argument) throws
-            IOException {
-        StringParser<P> parser = this.function.apply(context, argument);
+    public CommandArgumentResult<P> parse(CommandContext context, CommandArgumentContext<P> argument)
+            throws IOException {
+        StringParser<P> parser = this.function
+                .parse(context, new CommandArgumentContext<>(null, argument.getFirstArgument(), context.getCommand()))
+                .getValue();
         String arg = argument.getFocusArgument();
         P value = parser.parse(arg).orElseThrow(() -> new IOException(this.failMessage.apply(argument, parser)));
         return CommandArgumentResult.from(argument, value);
@@ -42,7 +45,15 @@ public class StringParserArgument<P> implements CommandArgument<P> {
 
     @Override
     public Set<String> suggest(CommandContext commandContext, CommandArgumentContext<P> argument) {
-        StringParser<P> parser = this.function.apply(commandContext, argument);
+        StringParser<P> parser;
+        try {
+            parser = this.function
+                    .parse(commandContext,
+                           new CommandArgumentContext<>(null, argument.getFirstArgument(), commandContext.getCommand()))
+                    .getValue();
+        } catch (IOException e) {
+            return Collections.emptySet();
+        }
         String peek = argument.getFocusArgument();
         if (!(parser instanceof StringParser.Suggestible)) {
             return Collections.emptySet();
